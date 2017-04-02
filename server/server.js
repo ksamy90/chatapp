6487222567365e4408c970f5defcc75fc198df30
samson <ksamy90@aol.com>
2017-04-02 07:47:54 +0000

    Initial Commit

---
+++ server/server.js
@@ -0,0 +1,12 @@
+const path = require('path');
+const express = require('express');
+const publicPath = path.join(__dirname, '../public');
+const port = process.env.PORT || 8080;
+
+var app = express();
+
+app.use(express.static(publicPath));
+
+app.listen(port, () =>{
+	console.log(`Server Started on port ${port}`);
+});
