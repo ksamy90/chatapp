8aa5989e7404ca2841019d2582e4d71e58c815d2
samson <ksamy90@aol.com>
2017-04-02 12:27:35 +0000

    Greet new user and alert others

--- server/server.js
+++ server/server.js
@@ -13,6 +13,16 @@ app.use(express.static(publicPath));
 
 io.on('connection', (socket) =>{
 	console.log('New user connected');
+	socket.emit('newMessage',{
+		from:'Admin',
+		text:'Welcome to Chat app',
+		createdAt: new Date().getTime()
+	});
+	socket.broadcast.emit('newMessage',{
+		from:'Admin',
+		text:'New user joined',
+		createdAt: new Date().getTime()
+	});
 	
 	socket.on('createMessage', (message) =>{
 		console.log('createMessage',message);
@@ -21,6 +31,11 @@ io.on('connection', (socket) =>{
 			text: message.text,
 			createdAt: new Date().getTime()
 		});
+		// socket.broadcast.emit('newMessage',{
+		// 	from: message.from,
+		// 	text: message.text,
+		// 	createdAt: new Date().getTime()
+		// });
 	});
 	socket.on('disconnect', () =>{
     		console.log('User was disconnected');
